8003a71e2a50fa0a5003269e8dbd442831bde810
gaetan <gaetan.chambouvet@gmail.com>
2019-02-10 21:15:26 +0100

    Improving design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,7 @@ export default class Navbar extends React.Component {
     return (
       <Menu id="top-navbar" className="fixed">
         <Menu.Item>
+          <img src="images/polydrive-icon.png" className="logo-navbar" alt="Logo"/>
           <Header id="brand-name" as='h1'>PolyDrive</Header>
         </Menu.Item>
         <Icon name='log out' className='log-out-icon' onClick={this.handleClickLogout}/>
